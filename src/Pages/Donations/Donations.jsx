0155a9c3fe041a03e40a17053d8befa5ba14c516
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-09-26 13:41:59 +0600

    created my donations page and added styling dynamically on the basis of json

--- src/Pages/Donations/Donations.jsx
+++ src/Pages/Donations/Donations.jsx
@@ -1,9 +1,15 @@
+import MyDonationCard from "./MyDonationCard";
 
 
 const Donations = () => {
+    const myDonations = JSON.parse(localStorage.getItem('donation'));
+    console.log(myDonations);
+    
     return (
-        <div>
-            
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {
+                myDonations.map(element => <MyDonationCard key={element.id} donation={element}></MyDonationCard>)
+            }        
         </div>
     );
 };
---
+++ src/Pages/Donations/MyDonationCard.jsx
@@ -0,0 +1,22 @@
+
+
+const MyDonationCard = ({donation}) => {
+    const {title,picture, category, price, bg_color, text_color, category_color} = donation;
+    return (
+        <div className="mt-5">
+            <div className="flex justify-between items-center rounded-md" style={{backgroundColor:bg_color}}>
+                <div className="">
+                    <img src={picture} alt="" className="rounded-md" />
+                </div>
+                <div className="p-4 space-y-2">
+                    <p className="text-sm inline-block p-0.5 rounded-md" style={{color:text_color, backgroundColor:category_color}}>{category}</p>
+                    <h1 className="text-2xl font-medium">{title}</h1>
+                    <p className="font-bold text-xl" style={{color:text_color}}>${price}</p>
+                    <button className="p-2 rounded-md font-medium text-white" style={{backgroundColor:text_color}}>View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyDonationCard;
